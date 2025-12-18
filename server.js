8cd681927a79e9bf206ec13a564fe78adfc1a9aa
sanhsing <sanhsing@gmail.com>
2025-12-18 22:35:09 +0800

    fix: CORS允許landing域名 + ECPay表單HTML

--- server.js
+++ server.js
@@ -35,7 +35,8 @@ app.use(cors({
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'https://sanhsing.github.io',
-    'https://beidou.edu.tw'
+    'https://beidou.edu.tw',
+    'https://beidou-landing.onrender.com'
   ],
   credentials: true
 }));
